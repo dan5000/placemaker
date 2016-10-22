1ce269953a233a06db01cf948f16aded0b4a7a6f
Joe Sutherland <jls124@icloud.com>
2016-10-22 17:11:59 -0500

    hooking up api

--- placemaker/www/js/controllers.js
+++ placemaker/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, CoC) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,10 +11,8 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.chats = CoC.all();
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
--- placemaker/www/js/services.js
+++ placemaker/www/js/services.js
@@ -1,50 +1,30 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    .service('CoC', function ($http) {
+        var service = this,
+            baseUrl = '/api/coc/';
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        function getUrl() {
+            return 'http://localhost:5000' + baseUrl;
         }
-      }
-      return null;
-    }
-  };
-});
+
+        service.all = function () {
+            return $http.get(getUrl() + "all/");
+        };
+
+        service.fetch = function (id) {
+            return $http.get(getUrl() + "read/" + id);
+        };
+
+        // service.create = function (object) {
+        //     return $http.post(getUrl(), object);
+        // };
+
+        // service.update = function (id, object) {
+        //     return $http.put(getUrlForId(id), object);
+        // };
+
+        // service.delete = function (id) {
+        //     return $http.delete(getUrlForId(id));
+        // };
+    });
