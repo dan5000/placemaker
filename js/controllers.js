28bcdced4c47ef493b7e82f5bcf52b312afe4fb5
Dan Salmo <dansalmo@gmail.com>
2016-10-22 23:06:41 -0500

    icon

--- js/controllers.js
+++ js/controllers.js
@@ -29,99 +29,18 @@ $scope.fname = 'atatat';
 
 })
 
-.controller('AdminDashCtrl', function (ItemsModel, $rootScope) {
+.controller('AdminDashCtrl', function (Persons, $rootScope, $scope) {
     var vm = this;
+    Persons.all()
+        .then(function (result) {
+            $scope.persons = JSON.parse(result.data);
+        });
 
-    function getAll() {
-        ItemsModel.all()
-            .then(function (result) {
-                vm.data = result.data.data;
-            });
-    }
-
-    function clearData() {
-        vm.data = null;
-    }
-
-    function create(object) {
-        ItemsModel.create(object)
-            .then(function (result) {
-                cancelCreate();
-                getAll();
-            });
-    }
-
-    function update(object) {
-        ItemsModel.update(object.id, object)
-            .then(function (result) {
-                cancelEditing();
-                getAll();
-            });
-    }
-
-    function deleteObject(id) {
-        ItemsModel.delete(id)
-            .then(function (result) {
-                cancelEditing();
-                getAll();
-            });
-    }
-
-    function initCreateForm() {
-        vm.newObject = {
-            name: '',
-            description: ''
-        };
-    }
-
-    function setEdited(object) {
-        vm.edited = angular.copy(object);
-        vm.isEditing = true;
-    }
-
-    function isCurrent(id) {
-        return vm.edited !== null && vm.edited.id === id;
-    }
-
-    function cancelEditing() {
-        vm.edited = null;
-        vm.isEditing = false;
-    }
-
-    function cancelCreate() {
-        initCreateForm();
-        vm.isCreating = false;
-    }
-
-    vm.objects = [];
-    vm.edited = null;
-    vm.isEditing = false;
-    vm.isCreating = false;
-    vm.getAll = getAll;
-    vm.create = create;
-    vm.update = update;
-    vm.delete = deleteObject;
-    vm.setEdited = setEdited;
-    vm.isCurrent = isCurrent;
-    vm.cancelEditing = cancelEditing;
-    vm.cancelCreate = cancelCreate;
-    vm.isAuthorized = false;
-
-    $rootScope.$on('authorized', function () {
-        vm.isAuthorized = true;
-        getAll();
-    });
-
-    $rootScope.$on('logout', function () {
-        clearData();
-    });
-
-    if (!vm.isAuthorized) {
-        $rootScope.$broadcast('logout');
-    }
-
-    initCreateForm();
-    getAll();
+    // 
+    // $rootScope.$on('authorized', function () {
+    //     vm.isAuthorized = true;
+    //     getAll();
+    // });
 
 })
 
